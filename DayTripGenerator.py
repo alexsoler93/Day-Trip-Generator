7c4a758920c462cd4516bec934dbd712ffc119b1
AlexSoler93 <a.soler12493@yahoo.com>
2022-10-03 04:27:37 -0500

    Compiled lists for day trip generator

---
+++ DayTripGenerator.py
@@ -0,0 +1,12 @@
+from random import Random, random
+
+
+destinations = ['Las Vegas', 'New York City', 'Orlando', 'Chicago', 'Nashville']
+
+restaurants = ['Pizza Time', 'Taco Tuesday', 'Ramen Bowls', 'Wings Wing Wings', 'Homestyle Cookin']
+
+entertainment = ['Music Concert', 'Sight Seeing', 'Watch a Play', 'Visit the Museum', 'Amusement Park']
+
+transportation = ['Moped Scooter', 'Luxury Car', 'Helicopter', 'Horseback', 'Roller Skates']
+
+def run_genertor():
