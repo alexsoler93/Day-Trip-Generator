73b773553f835b78e5397333837f9edfb84298c7
AlexSoler93 <a.soler12493@yahoo.com>
2022-10-05 07:34:30 -0500

    Created a function to pick a destinaton with a little added prompt

--- DayTripGenerator.py
+++ DayTripGenerator.py
@@ -1,5 +1,4 @@
-from random import Random, random
-
+import random
 
 destinations = ['Las Vegas', 'New York City', 'Orlando', 'Chicago', 'Nashville']
 
@@ -9,4 +8,32 @@ entertainment = ['Music Concert', 'Sight Seeing', 'Watch a Play', 'Visit the Mus
 
 transportation = ['Moped Scooter', 'Luxury Car', 'Helicopter', 'Horseback', 'Roller Skates']
 
-def run_genertor():
+
+# def generate_trip():
+#     ran_destination = print(random.choice(destinations))
+
+#     ran_restaurants = print(random.choice(restaurants))
+
+#     ran_entertainment = print(random.choice(entertainment))
+
+#     ran_transportation = print(random.choice(transportation))
+
+def pick_a_place():
+    ran_dest = random.choice(destinations)
+    if ran_dest == "Las Vegas":
+        print("Las Vegas: Viva Las Vegas")
+    elif ran_dest == "New York City":
+        print("New York City: Hello Big Apple!")
+    elif ran_dest == "Orlando":
+        print("Orlando: Fun in the sun")
+    elif ran_dest == "Chicago":
+        print("Chicago: The Windy City!")
+    elif ran_dest == "Nashville":
+        print("Nashville: There's nothing a trip here can't solve!")
+    else:
+        print()
+
+pick_a_place()
+
+
+
